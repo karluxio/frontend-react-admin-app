c70df4a389d30273f50e53b8e482f2f05b6a2725
karlux <karlux.sistemas@gmail.com>
2023-07-28 17:04:13 -0300

    feat: add Header component to Dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,3 +1,12 @@
+import { Header } from "@/components";
+import { Box } from "@mui/material";
+
 export const Dashboard = () => {
-  return <h2>Dashboard</h2>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
 };
