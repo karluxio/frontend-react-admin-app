4e977d2bfddad7855fba56c91f89a968aad2cd4e
karlux <karlux.sistemas@gmail.com>
2023-08-04 00:24:59 -0300

    feat: add data grid to Invoices scene with mockData

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,22 @@
-import { Contacts, Dashboard, Team } from "@/scenes";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Contacts, Dashboard, Invoices, Team } from '@/scenes'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/team" element={<Team />} />
-      <Route path="/contact" element={<Contacts />} />
-      <Route path="/invoices" element={<h2>invoices</h2>} />
-      <Route path="/form" element={<h2>form</h2>} />
-      <Route path="/bar" element={<h2>bar</h2>} />
-      <Route path="/pie" element={<h2>pie</h2>} />
-      <Route path="/line" element={<h2>line</h2>} />
-      <Route path="/faq" element={<h2>faq</h2>} />
-      <Route path="/geography" element={<h2>geography</h2>} />
-      <Route path="/calendar" element={<h2>calendar</h2>} />
+      <Route path='/' element={<Dashboard />} />
+      <Route path='/team' element={<Team />} />
+      <Route path='/contact' element={<Contacts />} />
+      <Route path='/invoices' element={<Invoices />} />
+      <Route path='/form' element={<h2>form</h2>} />
+      <Route path='/bar' element={<h2>bar</h2>} />
+      <Route path='/pie' element={<h2>pie</h2>} />
+      <Route path='/line' element={<h2>line</h2>} />
+      <Route path='/faq' element={<h2>faq</h2>} />
+      <Route path='/geography' element={<h2>geography</h2>} />
+      <Route path='/calendar' element={<h2>calendar</h2>} />
 
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path='*' element={<Navigate to='/' />} />
     </Routes>
-  );
-};
+  )
+}
